75763f82fdd2ed1c4b88a921e52266d7efbcd628
Paul Ascenzi <pfa5007@gmail.com>
2020-03-22 11:40:55 -0400

    first lambdas

---
+++ lambda/UserCreate/index.js
@@ -0,0 +1,26 @@
+const { Pool } = require("pg");
+const pool = new Pool();
+pool.connect();
+
+exports.handler = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  var body = JSON.parse(event.body)
+  var username = [body['username']]
+  var password = [body['password']]
+
+  pool.query(
+    'INSERT INTO public."UserPrivate"("Username", "Password") VALUES ($1, $2);',
+    [username, password],
+    (err, res) => {
+      var response = {
+        "statusCode": 200,
+        "isBase64Encoded": false,
+        "headers": {
+          "Access-Control-Allow-Origin": "*"
+        },
+        "body": "Successfully created User" + username
+      }
+      callback(err, res);
+    }
+  );
+};
