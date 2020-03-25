76513666e98e8ba8e0025177549c869a456c8c98
Paul Ascenzi <pfa5007@gmail.com>
2020-03-25 09:15:35 -0400

    updated user creation

--- angular-app/src/app/user-create/user-create.component.ts
+++ angular-app/src/app/user-create/user-create.component.ts
@@ -15,6 +15,8 @@ export class UserCreateComponent implements OnInit {
   public password: FormControl = new FormControl();
   public email: FormControl = new FormControl();
 
+  public failedUsername: boolean = false;
+  public failedPassword: boolean = false;
   public failedCreate: boolean = false;
 
   constructor(private userService: UserService, private router: Router, private formBuilder: FormBuilder) {
@@ -29,9 +31,14 @@ export class UserCreateComponent implements OnInit {
   }
 
   public onSubmit() {
-    if (this.username == null || this.password == null) {
-      this.form.reset();
-      this.failedCreate = true;
+    console.log(this.username.value + " " + this.password.value)
+    if (this.username.value == null || this.username.value == "") {
+      this.failedUsername = true;
+    }
+    if (this.password.value == null || this.password.value == "") {
+      this.failedPassword = true;
+    }
+    if (this.failedUsername || this.failedPassword) {
       return;
     }
     this.userService.createNewUser(this.username.value, this.password.value, this.email.value).subscribe(resp => {
